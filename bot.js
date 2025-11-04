099343e318a9266261bbe7361abc5a495949acde
kindowner02 <kindowner02@gmail.com>
2025-11-04 16:53:18 +0530

    bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const mineflayer = require('mineflayer')
+const antiafk = require('@emberglaze/mineflayer-anti-afk')
+
+const bot = mineflayer.createBot({
+  host: 'The_Demolisher.aternos.me', // Apna Aternos IP likh (jaise yourname.aternos.me)
+  port: 25565,                 // port agar alag ho to change kar
+  username: 'kindowner02@gmail.com', // Minecraft alt email ya username
+  // password: 'yourpassword'   // Mojang accounts ke liye hi
+})
+
+bot.once('spawn', () => {
+  console.log('✅ Bot joined! Anti-AFK enabled.')
+  antiafk(bot)
+})
+
+bot.on('kicked', (reason) => console.log('Kicked:', reason))
+bot.on('error', console.log)
